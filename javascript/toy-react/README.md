4d1e60d100a45f13ac740adca7092d9370319614
luziwei <zwlu@wisedu.com>
2020-09-08 18:04:19 +0800

    toy-react

---
+++ javascript/toy-react/README.md
@@ -0,0 +1,9 @@
+# TODO
+
+1. 先学习一遍react的tutorial，再来实现toy-react
+1. 理解class的getter和setter
+2. 理解正则表达式
+3. 理解递归
+4. 理解Symbol
+5. 理解dom的range相关api
+6. 理解深拷贝
--- javascript/toy-react/toy-react.js
+++ javascript/toy-react/toy-react.js
@@ -1,18 +1,45 @@
+const RENDER_TO_DOM = Symbol('render to dom')
+
 class ElementWrapper {
   constructor(type) {
     this.root = document.createElement(type)
   }
   setAttribute(name, value) {
-    this.root.setAttribute(name, value)
+    // 以on开头的所有字符，[\s\S]表示所有字符，$表示匹配符
+    if (name.match(/^on([\s\S]+)$/)) {
+      this.root.addEventListener(
+        // 将第一个字母替换成小写
+        RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()),
+        value
+      )
+    } else {
+      if (name === 'className') {
+        this.root.setAttribute('class', value)
+      } else {
+        this.root.setAttribute(name, value)
+      }
+    }
   }
   appendChild(component) {
-    this.root.appendChild(component.root)
+    let range = document.createRange()
+    range.setStart(this.root, this.root.childNodes.length)
+    range.setEnd(this.root, this.root.childNodes.length)
+    component[RENDER_TO_DOM](range)
+    // this.root.appendChild(component.root)
+  }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents()
+    range.insertNode(this.root)
   }
 }
 
 class TextWrapper {
   constructor(content) {
     this.root = document.createTextNode(content)
+  }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents()
+    range.insertNode(this.root)
   }
 }
 
@@ -22,6 +49,7 @@ export class Component {
     this.props = Object.create(null)
     this.children = []
     this._root = null
+    this._range = null
   }
   setAttribute(name, value) {
     this.props[name] = value
@@ -29,11 +57,52 @@ export class Component {
   appendChild(component) {
     this.children.push(component)
   }
-  get root() {
-    if (!this._root) {
-      this._root = this.render().root
+  // 被RENDER_TO_DOM替代
+  // get root() {
+  //   if (!this._root) {
+  //     this._root = this.render().root
+  //   }
+  //   return this._root
+  // }
+  // 递归
+  [RENDER_TO_DOM](range) {
+    this._range = range
+    this.render()[RENDER_TO_DOM](range)
+  }
+  rerender() {
+    // 保存老的range
+    let oldRange = this._range
+
+    // 创建新的range，设为老的range的开头，在新的range插入
+    let range = document.createRange()
+    range.setStart(oldRange.startContainer, oldRange.startOffset)
+    range.setEnd(oldRange.startContainer, oldRange.startOffset)
+    this[RENDER_TO_DOM](range)
+
+    // 将老的range放到插入的内容之后，然后删除
+    oldRange.setStart(range.endContainer, range.endOffset)
+    oldRange.deleteContents()
+  }
+  // 递归
+  setState(newState) {
+    // typeof必须和null判断一起使用，因为typeof null === object
+    if (this.state === null || typeof this.state !== 'object') {
+      this.state = newState
+      this.rerender()
+      return
     }
-    return this._root
+    let merge = (oldState, newState) => {
+      for (let p in newState) {
+        // 深拷贝
+        if (oldState[p] === null || typeof oldState[p] !== 'object') {
+          oldState[p] = newState[p]
+        } else {
+          merge(oldState[p], newState[p])
+        }
+      }
+    }
+    merge(this.state, newState)
+    this.rerender()
   }
 }
 
@@ -54,6 +123,9 @@ export function createElement(type, attributes, ...children) {
       if (typeof child === 'string') {
         child = new TextWrapper(child)
       }
+      if (child === null) {
+        continue
+      }
       if (typeof child === 'object' && child instanceof Array) {
         insertChildren(child)
       } else {
@@ -67,5 +139,9 @@ export function createElement(type, attributes, ...children) {
 }
 
 export function render(component, parentElement) {
-  parentElement.appendChild(component.root)
+  let range = document.createRange()
+  range.setStart(parentElement, 0)
+  range.setEnd(parentElement, parentElement.childNodes.length)
+  range.deleteContents()
+  component[RENDER_TO_DOM](range)
 }
