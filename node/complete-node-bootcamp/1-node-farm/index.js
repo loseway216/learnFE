07477b2970f15904b028eed3bd9f2c651adbe624
loseway216 <loseway216@gmail.com>
2020-09-22 23:08:15 +0800

    node

--- node/complete-node-bootcamp/1-node-farm/index.js
+++ node/complete-node-bootcamp/1-node-farm/index.js
@@ -4,6 +4,7 @@ const url = require('url')
 
 /////////////////////////////////////
 // FILES
+/*
 console.log('------------files-----------')
 
 // Blocking, synchronous way
@@ -13,7 +14,6 @@ console.log(textIn)
 const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`
 fs.writeFileSync('./txt/output.txt', textOut)
 console.log('File wirtten')
-
 // Non-blocking, asynchronous way
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
   if (err) return console.log('ERROR!')
@@ -26,24 +26,70 @@ fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
   })
 })
 console.log('Will read file')
+*/
 
 /////////////////////////////////////
 // SERVER
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%ID%}/g, product.id)
+  output = output.replace(/{%PRODUCTNAME%}/g, product.productName)
+  output = output.replace(/{%IMAGE%}/g, product.image)
+  output = output.replace(/{%FROM%}/g, product.from)
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+  output = output.replace(/{%QUANTITY%}/g, product.quantity)
+  output = output.replace(/{%PRICE%}/g, product.price)
+  output = output.replace(/{%DESCRIPTION%}/g, product.description)
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+
+  return output
+}
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+)
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+)
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+)
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data)
 
 console.log('------------server-----------')
 const server = http.createServer((req, res) => {
-  const pathName = req.url
+  // 用自带的url库解析url
+  const { query, pathname } = url.parse(req.url, true)
 
-  if (pathName === '/' || pathName === '/overview') {
-    res.end('This is the OVERVIEW')
-  } else if (pathName === '/product') {
-    res.end('This is the PRODUCT')
-  } else if (pathName === '/api') {
+  // Overview page
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, { 'Content-type': 'text/html' })
+
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('')
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml)
+    res.end(output)
+
+    // Product page
+  } else if (pathname === '/product') {
+    res.writeHead(200, { 'Content-type': 'text/html' })
+
+    const product = dataObj[query.id]
+    const productHtml = replaceTemplate(tempProduct, product)
+    res.end(productHtml)
+
+    // API
+  } else if (pathname === '/api') {
     res.writeHead(200, { 'Content-type': 'application/json' })
     res.end(data)
+
+    // Not found
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
