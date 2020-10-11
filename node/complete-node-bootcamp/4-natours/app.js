a56587113192642c41b551d5fb91550750b89636
loseway216 <loseway216@gmail.com>
2020-10-12 00:38:18 +0800

    node

---
+++ node/complete-node-bootcamp/4-natours/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+// 路由
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello from server side!', app: 'Natours' });
+});
+
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
