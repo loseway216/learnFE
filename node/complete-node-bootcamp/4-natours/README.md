b80894a87f01c40697712ad3e768bb3285411fe7
luziwei <zwlu@wisedu.com>
2020-10-12 18:18:55 +0800

    node

---
+++ node/complete-node-bootcamp/4-natours/README.md
@@ -0,0 +1,5 @@
+# 项目思路
+
+1. 定义路由，CRUD
+2. mongoDB
+3. mongoose
--- node/complete-node-bootcamp/4-natours/app.js
+++ node/complete-node-bootcamp/4-natours/app.js
@@ -1,16 +1,120 @@
+const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-// 路由
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from server side!', app: 'Natours' });
+// 1) MIDDLEWARE,顺序有影响
+
+// logging
+app.use(morgan('dev'));
+// body parser
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
 });
 
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoint...');
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
+// 2) HANDLERS
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+const getAllTours = (req, res) => {
+  console.log(req.requestTime);
+
+  res.status(200).json({
+    status: 'success',
+    requestAt: req.requestTime,
+    results: tours.length,
+    data: { tours }
+  });
+};
+
+const getTour = (req, res) => {
+  // String to Number
+  const id = req.params.id * 1;
+  const tour = tours.find(el => el.id === id);
+
+  // if (id > tours.length) {
+  if (!tour) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: { tour }
+  });
+};
+
+const createTour = (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  // 在event loop中永远不要使用sync方法
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    err => {
+      // 201:created
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour }
+      });
+    }
+  );
+};
+
+const updateTour = (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>'
+    }
+  });
+};
+
+const deleteTour = (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({ status: 'fail', message: 'Invalid ID' });
+  }
+
+  // 204:no content
+  res.status(204).json({
+    status: 'success',
+    data: null
+  });
+};
+
+// 3) ROUTER
+// app.get('/api/v1/tours', getAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.post('/api/v1/tours', createTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+// 4) START SERVICE
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
