ce797e0bcdf04b98fd6c4599f4c3c71012c7779b
luziwei <zwlu@wisedu.com>
2020-10-14 18:16:42 +0800

    node

--- node/complete-node-bootcamp/4-natours/server.js
+++ node/complete-node-bootcamp/4-natours/server.js
@@ -1,9 +1,9 @@
 const dotenv = require('dotenv');
-const app = require('./app');
-
 dotenv.config({ path: './config.env' });
 
-const port = 3000;
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
