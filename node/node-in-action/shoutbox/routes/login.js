596395a799f598290c20c1edad11ce8918cf4f02
loseway216 <loseway216@gmail.com>
2020-11-08 18:14:01 +0800

    getify

--- node/node-in-action/shoutbox/routes/login.js
+++
@@ -1,9 +0,0 @@
-const User = require('../models/user')
-
-exports.submit = (req,res,next) => {
-    const data = req.body.user;
-    User.authenticate(data.name,data.pass,(err,user) => {
-        if(err) return next(err)
-        
-    })
-}
